cae5a6a4d732af9b3e24c9b816f3687f8efaf567
mbachtob <mbachtob@marvingt.enseeiht.fr>
2016-05-20 19:30:56 +0200

    INFOTYPE ajouté

---
+++ MCS-etud-1516/src/mcs/tds/INFOTYPE.java
@@ -0,0 +1,19 @@
+package mcs.tds;
+
+import mcs.type.DTYPE;
+
+public class INFOTYPE implements INFO {
+	
+	protected DTYPE type;
+	
+	// constructeur
+	public INFOTYPE(DTYPE t) {
+		type = t;
+	}
+
+	// affichage
+	public String toString() {
+		return "; ALIAS sur : " + "type=" + type ;
+	}
+
+}
