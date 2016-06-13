0cc3bf28d0e4942e9b1993a17b87de93e34aac29
marbelot <marbelot@z6po.enseeiht.fr>
2016-06-13 17:02:48 +0200

    Debut implantation namesapce

--- MCS-etud-1516/src/mcs/tds/Namespace.java
+++ MCS-etud-1516/src/mcs/tds/Namespace.java
@@ -6,20 +6,25 @@ import mcs.type.DTYPE;
 
 public class Namespace implements INFO {
     
-    protected ArrayList<Namespace> sousnamespace;
+    protected ArrayList<String> sousnamespace;
    	protected TDS nstds ;
 	
-   	public Namespace(ArrayList<Namespace> sousnamespace, TDS nstds) {
+   	public Namespace(ArrayList<String> sousnamespace, TDS nstds) {
 		super();
 		this.sousnamespace = sousnamespace;
 		this.nstds = nstds;
 	}
-
+   	
+   	public Namespace(TDS nstds) {
+		super();
+		this.sousnamespace = null;
+		this.nstds = nstds;
+	}
 	public TDS getNstds() {
 		return nstds;
 	}
    	
-    public ArrayList<Namespace> getSousnamespace() {
+    public ArrayList<String> getSousnamespace() {
 		return sousnamespace;
 	}
     
